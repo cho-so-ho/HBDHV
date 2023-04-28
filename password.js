f4b2634566d90a2e6cfbb4133e5e98ec7e93d6d2
chosoho <94427306+cho-so-ho@users.noreply.github.com>
2023-04-29 02:01:37 +0900

    Add files via upload

---
+++ password.js
@@ -0,0 +1,11 @@
+
+var password = "03968"
+ function passcheck() {
+if(document.getElementById("03968"). value != password){
+alert("거기가 아니다");
+return false;
+}
+if(document.getElementById("03968"). value == password){
+alert("어서오세요");
+}
+ }   
